b18cfb70a7d684a004c6e64229a4309a29d6f09e
Yohannes Mekonnen <ymekonnen943@gmail.com>
2021-07-02 17:00:27 +0300

    Update post migration file

--- blog/app/User.php
+++ blog/app/User.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Model
 {
-    //
+    use HasApiTokens, HasFactory, Notifiable;
 }
